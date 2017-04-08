38f7617504ab337580bf11ea6a70540d4764b23d
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2017-04-08 11:18:11 +0300

    Add charset formatting to UTF-8

--- get_data.sh
+++ get_data.sh
@@ -3,5 +3,6 @@
 wget http://trafiopendata.97.fi/opendata/Veneet_1_7.zip
 unzip Veneet_1_7.zip
 rm Veneet_1_7.zip
-mkdir data
-mv avoindata1_7.csv data/data.csv
+mkdir -p data
+iconv -f iso-8859-1 -t utf8 avoindata1_7.csv > data/data.csv
+rm avoindata1_7.csv
